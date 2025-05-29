bf73510ce9186b2f49ebbb7b5739767d8b36ca7a
binf01-Trinari <105106999+binf01-Trinari@users.noreply.github.com>
2025-05-29 16:09:18 +0200

    Aggiornato mainclient.java

--- src/mainclient.java
+++ src/mainclient.java
@@ -1,2 +1,10 @@
-public class mainclient {
+public class MainClient {
+    public static void main(String[] args) {
+        Client client = new Client("Mario", "rosso");
+        if (client.connetti("localhost", 12345) == 0) {
+            client.scrivi("Ciao dal client!");
+            client.leggi();
+            client.chiudi();
+        }
+    }
 }
